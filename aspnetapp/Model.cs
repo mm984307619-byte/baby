c42dac7468c4523703c41d390960d9b6a1cca0d3
mm984307619-byte <mm984307619@gmail.com>
2025-09-09 21:55:28 +0800

    Update Model.cs

--- aspnetapp/Model.cs
+++ aspnetapp/Model.cs
@@ -7,4 +7,12 @@ namespace aspnetapp
         public DateTime createdAt { get; set; }
         public DateTime updatedAt { get; set; }
     }
+    public class Tip
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public string Link { get; set; }
+        public string Url { get; set; }
+    }
 }
